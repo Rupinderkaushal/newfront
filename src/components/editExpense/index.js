5c9ee21209b7751b65afbbde433a2c0414665648
Rupinder18 <rupi2003nder@gmail.com>
2024-02-26 20:08:38 +0530

    add id

--- src/components/editExpense/index.js
+++ src/components/editExpense/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const EditExpenses = () => {
+ const {id}= useParams();
    const  submitHandler=async()=>{
     alert("huu")
    };
